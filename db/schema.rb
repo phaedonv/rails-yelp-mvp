fef245a468a2f1a42fe3eff912c9f728837e2ac6
Phaedon Valasiadis <faival@protonmail.com>
2020-05-13 14:17:34 +0300

    I used scaffold 4 reviews..:(

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_095431) do
+ActiveRecord::Schema.define(version: 2020_05_13_103051) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_05_13_095431) do
     t.string "category"
     t.integer "phone"
     t.integer "reviews"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
